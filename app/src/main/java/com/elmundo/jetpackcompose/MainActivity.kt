31912801ab6fc4782c3e29c5cf27f669d29e3855
IsraelAIM <75389817+IsraelAIM@users.noreply.github.com>
2022-04-30 21:28:40 -0500

    Hecho

--- app/src/main/java/com/elmundo/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/elmundo/jetpackcompose/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -19,27 +21,59 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.elmundo.jetpackcompose.ui.theme.JetPackComposeTheme
 
+//Lista de mensajes
+private val messages: List<MyMessage> = listOf(
+    MyMessage("Hola Roberto", "¿Listo para ir de shop?"),
+    MyMessage("Hola Rey", "¿Listo para ir de shop?"),
+    MyMessage("Hola Asiw", "¿Listo para ir de shop?"),
+    MyMessage("Hola Saber", "¿Listo para ir de shop?"),
+    MyMessage("Hola Israel", "¿Listo para ir de shop?"),
+    MyMessage("Hola Tony", "¿Listo para ir de shop?"),
+    MyMessage("Hola Sharon", "¿Listo para ir de shop?"),
+    MyMessage("Hola Henry", "¿Listo para ir de shop?"),
+    MyMessage("Hola Jasón", "¿Listo para ir de shop?"),
+    MyMessage("Hola Connor", "¿Listo para ir de shop?"),
+    MyMessage("Hola Fred", "¿Listo para ir de shop?"),
+    MyMessage("Hola Cloe", "¿Listo para ir de shop?"),
+)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetPackComposeTheme {
-                MyComponent()
+                //Aqui se ejecuta y el que ejecuta son los mensajes precargados del privat val
+                MyMessages(messages)
             }
-
         }
     }
 }
 
+//El tipo de dato para poder enlistarlo
+data class MyMessage(val title: String, val body: String)
+
+//Esta clase es la que dará mensajes. messages será otorgado a items para cargar el listado, generando los elementos en pantalla y eliminando los que no.
 @Composable
-fun MyComponent() {
+fun MyMessages(messages: List<MyMessage>) {
+    LazyColumn {
+        items(messages) { message ->
+            MyComponent(message)
+        }
+    }
+}
+
+//El message de tipo MyMessage está hecho para recibir después en el metodo MyMessages los mensajes en el Lazy Colum, los items son recibidos del listado y cada item, message es asignado a cada MyComponent
+@Composable
+fun MyComponent(message: MyMessage) {
     //  modifier= Modifier.padding(top = 8.dp)
     //El orden de las cosas, primero se aplica el fondo y luego el padding
-    Row(modifier = Modifier
-        .background(MaterialTheme.colors.background)
-        .padding(8.dp)) {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .padding(8.dp)
+    ) {
         MyImage()
-        myTexts()
+        myTexts(message)
     }
 }
 
@@ -57,25 +91,27 @@ fun MyImage() {
     )
 }
 
+//El MyMessage es un tipo de dato y se les otorgado porque de él se le pondrá a los demás elementos
 @Composable
-fun myTexts() {
+fun myTexts(message: MyMessage) {
     Column(modifier = Modifier.padding(start = 8.dp)) {
         MyText(
-            "Hola Alvey",
+            message.title,
             MaterialTheme.colors.primary,
             MaterialTheme.typography.subtitle1
         )
         Spacer(modifier = Modifier.height(16.dp))
 
         MyText(
-            "¿Preparado?",
+            message.body,
             MaterialTheme.colors.onBackground,
             MaterialTheme.typography.subtitle2
         )
         MyText(
             "Iniciando el gran viaje",
             MaterialTheme.colors.primaryVariant,
-            MaterialTheme.typography.body1)
+            MaterialTheme.typography.body1
+        )
         MyText(
             "Será largo",
             MaterialTheme.colors.primaryVariant,
@@ -84,17 +120,26 @@ fun myTexts() {
     }
 
 }
-
+//Digamos que es una plantilla para un mensaje
 @Composable
 fun MyText(text: String, color: Color, style: androidx.compose.ui.text.TextStyle) {
     Text(text, color = color, style = style)
 }
-@Preview()
+
+//@Preview()
+@Preview(showSystemUi = true) //Para visualizar como si fuese telefono
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewComponent() {
     JetPackComposeTheme {
-        MyComponent()
+        MyMessages(messages) //Ejecuta el metodo para visualizar
+
+        //Añadir scroll al componente
+        /*val scrollState= rememberScrollState()
+        Column(modifier= Modifier.verticalScroll(scrollState)) {
+            MyMessages(messages)
+        }*/
+
     }
 
 }
